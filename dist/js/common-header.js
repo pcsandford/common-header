1defd623f95252fe8c54d4fbdcf4c3b786305b28
Alex Deaconu <alex.deaconu@gmail.com>
2015-02-17 14:36:10 -0500

    [fix] String concatenation

--- dist/js/common-header.js
+++ dist/js/common-header.js
@@ -3646,7 +3646,7 @@ angular.module("risevision.common.geodata", [])
           $location.replace();
         }
         if (state.p || state.s) {
-          $window.location.replace(state.p + state.s ? "?" + state.s : "");
+          $window.location.replace(state.p + (state.s ? "?" + state.s : ""));
         }
       }
       if (!resolveHandled) {
